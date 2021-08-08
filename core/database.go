558a8413b7b03e4b20952724b78a48e04aea547e
anand-kay <anand.kay95@gmail.com>
2021-08-08 15:21:50 +0530

    added Dockerfile

--- core/database.go
+++ core/database.go
@@ -11,6 +11,11 @@ import (
 // var db *gorm.DB
 
 func newDB() (*gorm.DB, error) {
+	// os.Setenv("db_host", "localhost")
+	// os.Setenv("db_user", "anandkay")
+	// os.Setenv("db_password", "mypostgres")
+	// os.Setenv("db_name", "linkedin")
+	// os.Setenv("db_port", "5432")
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", os.Getenv("db_host"), os.Getenv("db_user"), os.Getenv("db_password"), os.Getenv("db_name"), os.Getenv("db_port"))
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
--- core/server.go
+++ core/server.go
@@ -1,8 +1,16 @@
 package core
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/avishekmondal14/ikta/server"
 )
@@ -17,6 +25,9 @@ func NewServer() (*server.Server, error) {
 
 	server.DB = db
 
+	// os.Setenv("host", "localhost")
+	// os.Setenv("port", "4000")
+
 	server.HTTPServer = &http.Server{}
 	server.HTTPServer.Addr = os.Getenv("host") + ":" + os.Getenv("port")
 	server.HTTPServer.Handler = GetHandler(NewRouter(server))
@@ -29,5 +40,56 @@ func StartServer(server *http.Server) error {
 }
 
 func ShutdownServer(server *server.Server) {
-	// TODO
+	// Create channel to receive OS signal
+	c := make(chan os.Signal, 1)
+
+	// SIGINT (Ctrl+C) - Graceful shutdown
+	// SIGQUIT (Ctrl+\) - Quit process without graceful shutdown
+	// SIGKILL or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, syscall.SIGINT, syscall.SIGQUIT)
+
+	// Block until we receive our signal.
+	sig := <-c
+
+	if sig == syscall.SIGINT {
+		// os.Setenv("graceful_shutdown_timeout", "15")
+		gracefulShutdownTimeout, _ := strconv.ParseInt(os.Getenv("graceful_shutdown_timeout"), 10, 64)
+		// Create a deadline to wait for.
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(rand.Int63n(gracefulShutdownTimeout)))
+		defer cancel()
+		// Doesn't block if no connections, but will otherwise wait
+		// until the timeout deadline.
+
+		// Close database connection
+		sqlDB, err := server.DB.DB()
+		if err != nil {
+			log.Println("Error while trying to retrieve sql db from gorm. Quiting process.")
+			log.Println(err)
+			os.Exit(1)
+		}
+		err = sqlDB.Close()
+		if err != nil {
+			log.Println("Error while trying to close database connection. Quiting process.")
+			log.Println(err)
+			os.Exit(1)
+		}
+
+		// Shutdown server gracefully
+		err = server.HTTPServer.Shutdown(ctx)
+		if err != nil {
+			log.Println("Error while trying to shutdown server gracefully. Quiting process.")
+			log.Println(err)
+			os.Exit(1)
+		}
+		// Optionally, you could run server.httpServer.Shutdown in a goroutine and block on
+		// <-ctx.Done() if your application should wait for other services
+		// to finalize based on context cancellation.
+
+		fmt.Println()
+		log.Println("Shutting down server gracefully")
+		os.Exit(0)
+	} else if sig == syscall.SIGQUIT {
+		log.Println("Quiting process. No graceful shutdown.")
+		os.Exit(1)
+	}
 }
